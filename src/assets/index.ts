88388d8edcb09dcec1767b79a09ce2e4de1fa391
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-07-26 21:51:00 +0900

    coins added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,70 +1,92 @@
-import UST from './UST.json'
-import DollarPac from './$pac.json'
-import OneInch from './1inch.json'
-import TwoGive from './2give.json'
-import aave from './aave.json'
-import abt from './abt.json'
-import act from './act.json'
-import actn from './actn.json'
-import ada from './ada.json'
-import add from './add.json'
-import adx from './adx.json'
-import ae from './ae.json'
-import aeon from './aeon.json'
-import agi from './agi.json'
-import agrs from './agrs.json'
-import aion from './aion.json'
-import albt from './albt.json'
-import algo from './algo.json'
-import amb from './amb.json'
-import amp from './amp.json'
-import ampl from './ampl.json'
-import ant from './ant.json'
-import apex from './apex.json'
-import appc from './appc.json'
-import ardr from './ardr.json'
-import arg from './arg.json'
-import ark from './ark.json'
-import arn from './arn.json'
-import arnx from './arnx.json'
-import ary from './ary.json'
-import ast from './ast.json'
-import atm from './atm.json'
-import atom from './atom.json'
-import aud from './aud.json'
-import audr from './audr.json'
-import auto from './auto.json'
-import aywa from './aywa.json'
-import bab from './bab.json'
-import bal from './bal.json'
-import band from './band.json'
-import bcbc from './bcbc.json'
-import bch from './bch.json'
-import bcio from './bcio.json'
-import bcn from './bcn.json'
-import bco from './bco.json'
-import bcpt from './bcpt.json'
-import bdl from './bdl.json'
-import beam from './beam.json'
-import bela from './bela.json'
-import bix from './bix.json'
-import blcn from './blcn.json'
-import blk from './blk.json'
-import block from './block.json'
-import blz from './blz.json'
-import bnb from './bnb.json'
-import bnt from './bnt.json'
-import bnty from './bnty.json'
-import booty from './booty.json'
-import bos from './bos.json'
-import bpt from './bpt.json'
-import bq from './bq.json'
-import brd from './brd.json'
-import bsd from './bsd.json'
-import bsv from './bsv.json'
-import btc from './btc.json'
-import btcd from './btcd.json'
-import btch from './btch.json'
+import UST from './UST.json';
+import DollarPac from './$pac.json';
+import OneInch from './1inch.json';
+import TwoGive from './2give.json';
+import aave from './aave.json';
+import abt from './abt.json';
+import act from './act.json';
+import actn from './actn.json';
+import ada from './ada.json';
+import add from './add.json';
+import adx from './adx.json';
+import ae from './ae.json';
+import aeon from './aeon.json';
+import agi from './agi.json';
+import agrs from './agrs.json';
+import aion from './aion.json';
+import albt from './albt.json';
+import algo from './algo.json';
+import amb from './amb.json';
+import amp from './amp.json';
+import ampl from './ampl.json';
+import ant from './ant.json';
+import apex from './apex.json';
+import appc from './appc.json';
+import ardr from './ardr.json';
+import arg from './arg.json';
+import ark from './ark.json';
+import arn from './arn.json';
+import arnx from './arnx.json';
+import ary from './ary.json';
+import ast from './ast.json';
+import atm from './atm.json';
+import atom from './atom.json';
+import aud from './aud.json';
+import audr from './audr.json';
+import auto from './auto.json';
+import aywa from './aywa.json';
+import bab from './bab.json';
+import bal from './bal.json';
+import band from './band.json';
+import bcbc from './bcbc.json';
+import bch from './bch.json';
+import bcio from './bcio.json';
+import bcn from './bcn.json';
+import bco from './bco.json';
+import bcpt from './bcpt.json';
+import bdl from './bdl.json';
+import beam from './beam.json';
+import bela from './bela.json';
+import bix from './bix.json';
+import blcn from './blcn.json';
+import blk from './blk.json';
+import block from './block.json';
+import blz from './blz.json';
+import bnb from './bnb.json';
+import bnt from './bnt.json';
+import bnty from './bnty.json';
+import booty from './booty.json';
+import bos from './bos.json';
+import bpt from './bpt.json';
+import bq from './bq.json';
+import brd from './brd.json';
+import bsd from './bsd.json';
+import bsv from './bsv.json';
+import btc from './btc.json';
+import btcd from './btcd.json';
+import btch from './btch.json';
+import btcp from './btcp.json';
+import btcz from './btcz.json';
+import btdx from './btdx.json';
+import btg from './btg.json';
+import btm from './btm.json';
+import bts from './bts.json';
+import btt from './btt.json';
+import btx from './btx.json';
+import burst from './burst.json';
+import bze from './bze.json';
+import call from './call.json';
+import cc from './cc.json';
+import cdn from './cdn.json';
+import cdt from './cdt.json';
+import cenz from './cenz.json';
+import chat from './chat.json';
+import chips from './chips.json';
+import clam from './clam.json';
+import cloak from './cloak.json';
+import cmm from './cmm.json';
+import cmt from './cmt.json';
+import cnd from './cnd.json';
 
 export {
   UST,
@@ -97,7 +119,7 @@ export {
   arn,
   arnx,
   ary,
-  ast, 
+  ast,
   atm,
   atom,
   aud,
@@ -134,5 +156,26 @@ export {
   btc,
   btcd,
   btch,
-  
-}
+  btcp,
+  btcz,
+  btdx,
+  btg,
+  btm,
+  bts,
+  btt,
+  btx,
+  burst,
+  bze,
+  call,
+  cc,
+  cdn,
+  cdt,
+  cenz,
+  chat,
+  chips,
+  clam,
+  cloak,
+  cmm,
+  cmt,
+  cnd,
+};
